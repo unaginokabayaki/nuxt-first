a27377bb686da1d2a51410be66171b48b130c02f
hitsuji <kyoukarada@gmail.com>
2024-03-22 12:09:49 +0900

    pages

---
+++ pages/blog.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>ブログページ</h1>
+    <p>こんにちは</p>
+</template>
+
+<script setup></script>
+
+<style>
+h1 {
+    color: red;
+}
+.text {
+    color: green;
+    letter-spacing: 20px;
+}
+</style>
