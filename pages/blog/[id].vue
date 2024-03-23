2db04ff2b757101a6974159a9415f0d1f0b24539
hitsuji <kyoukarada@gmail.com>
2024-03-23 13:59:52 +0900

    article

---
+++ pages/blog/[id].vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>記事ページです</h1>
+    <div>
+        <nuxt-img :src="item.image" alt="profile" width="500px"/>
+    </div>
+    <div>
+        {{ item.title }}
+        {{ item.date }}
+        <ContentDoc />
+    </div>
+
+</template>
+
+<script setup>
+const { data: item } = await useAsyncData(useRoute().path, () => 
+    queryContent(useRoute().path).findOne()
+)
+
+// useAsyncDataの名前は重複してはいけない
+// const { data: item } = await useAsyncData("/blog/blog-1", () => 
+//     queryContent(/blog/blog-1).findOne()
+// )
+
+console.log(useRoute().path)
+</script>
